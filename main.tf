f80ebbfa0a1653bdd4d1cf42c58005413ec61d79
krishnarajuy <krishnaraju6302@gmail.com>
2025-01-08 16:35:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "three" {
-  ami           = "ami-0866a3c8686eaeeba"
+  ami           = "ami-01816d07b1128cd2d"
   instance_type = "t2.micro"
   tags = {
     Name = "dev-server"
